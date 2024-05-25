6207e33d4196640289ff951c943a7f7e3fd97f5c
Liew Wei Cheng <lwc1488@yahoo.com.my>
2024-05-26 01:18:04 +0800

    added "things"

--- EndScene.js
+++ EndScene.js
@@ -4,23 +4,53 @@ class EndScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.spritesheet( 'codey_sled', 'images/codey_sled.png', { frameWidth: 81, frameHeight: 90 } );
+      this.load.image('platform', 'images/platform.png');
+      this.load.image('bg1', 'images/mountain.png');
+      this.load.image('bg2', 'images/trees.png');
+      this.load.image('bg3', 'images/snowdunes.png');
+      this.load.image('logo2', 'images/doggie-run-logo.png');
+      this.load.image('thanks', 'images/thanks-msg.png');
+
+      this.load.spritesheet('campfire', 'images/campfire.png', { frameWidth: 32, frameHeight: 32 });
+      this.load.spritesheet('doggy', 'images/doggy.png', { frameWidth: 48, frameHeight: 48 });
   }
 
   create() {
-    gameState.player = this.add.sprite(config.width / 2, config.height / 2, 'codey_sled');
+      this.add.image(0, 0, 'bg1').setOrigin(0, 0);
+      this.add.image(0, 0, 'bg2').setOrigin(0, 0);
+      this.add.image(0, 0, 'bg3').setOrigin(0, 0);
+      this.add.image((config.width / 2) - 100, 420, 'platform').setOrigin(0, 0);
+      this.add.image((config.width / 2) - 235, (config.height / 2) - 270, 'logo2').setOrigin(0);
+      this.add.image((config.width / 2) - 120, (config.height / 2), 'thanks').setOrigin(0);
 
-    this.anims.create( {
-                         key: 'sled',
-                         frames: this.anims.generateFrameNumbers('codey_sled'),
-                         frameRate: 10,
-                         repeat: -1
-                       } )
+      gameState.player = this.add.sprite((config.width / 2) - 15, 405, 'doggy');
+      gameState.player.setScale(0.25);
 
-    gameState.player.angle = 20;
+      gameState.campfire = this.add.sprite((config.width / 2) + 20, 405, 'campfire');
+
+        this.anims.create({
+            key: 'doggy',
+            frames: this.anims.generateFrameNumbers('doggy', { frames: [0, 4, 12, 8] }),
+            frameRate: 4,
+            repeat: -1
+        });
+
+      this.anims.create({
+          key: 'campfire',
+          frames: this.anims.generateFrameNumbers('campfire', { frames: [0,1,2] }),
+          frameRate: 4,
+          repeat: -1
+      });
+
+      this.input.keyboard.on('keydown', () => {
+          this.scene.stop('EndScene');
+          this.scene.start('StartScene');
+      });
+
   }
 
   update() {
-    gameState.player.anims.play('sled', true);
+      gameState.player.anims.play('doggy', true);
+      gameState.campfire.anims.play('campfire', true);
   }
 }
--- StartScene.js
+++ StartScene.js
@@ -4,31 +4,41 @@ class StartScene extends Phaser.Scene {
   }
 
   preload() {
-      this.load.image('platform', 'images/platform.png');
-      this.load.image('snowflake', 'images/snowflake.png');
       this.load.image('bg1', 'images/mountain.png');
       this.load.image('bg2', 'images/trees.png');
       this.load.image('bg3', 'images/snowdunes.png');
+      this.load.image('logo', 'images/doggie-run-logo2.png');
+
+      //this.load.spritesheet('doggy', 'images/doggy.png', { frameWidth: 48, frameHeight: 48 });
   }
 
    create() {
-    const screen = this.add.image(0, 0, 'start').setOrigin(0);
+       this.add.image(0, 0, 'bg1').setOrigin(0, 0);
+       this.add.image(0, 0, 'bg2').setOrigin(0, 0);
+       this.add.image(0, 0, 'bg3').setOrigin(0, 0);
 
-      this.add.image(0, 0, 'bg1').setOrigin(0, 0);
-      this.add.image(0, 0, 'bg2').setOrigin(0, 0);
-      this.add.image(0, 0, 'bg3').setOrigin(0, 0);
+       this.add.image((config.width / 2) - 150, (config.height / 2) - 130, 'logo').setOrigin(0).setScale(1.2);
 
-      let title = this.add.text(200, 250, "Doggie Run");
-      let playButton = this.add.text(226, 330, "Play");
+       //gameState.dog = this.add.sprite((config.width / 2), (config.height / 2) + 20, 'doggy');
+       //gameState.dog.setScale(0.75);
 
-       title.setColor(rgb(0, 48, 143));
-       playButton.setColor(rgb(0, 48, 143));
+       //this.anims.create({
+       //    key: 'doggy',
+       //    frames: this.anims.generateFrameNumbers('doggy', { frames: [0, 1, 2, 3] }),
+       //    frameRate: 10,
+       //    repeat: -1
+       //});
 
-    // on keypress any, transition to GameScene
-    this.input.keyboard.on('keydown', () => {
-      this.scene.stop('StartScene');
-      this.scene.start('GameScene');
-    });
-  }
+       // on keypress any, transition to GameScene
+       this.input.keyboard.on('keydown', () => {
+           this.scene.stop('StartScene');
+           this.scene.start('GameScene');
+       });
+    }
+
+    //update() {
+    //    gameState.dog.anims.play('doggy', true);
+    //}
+
 }
 
